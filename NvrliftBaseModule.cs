5b8f2b1f2582db9aa61cfc20f7e57cb6aea49328
Nico Macioszek <nico.macioszek@untitled.software>
2023-10-05 13:15:56 +0200

    :)

--- NvrliftBaseModule.cs
+++ NvrliftBaseModule.cs
@@ -19,7 +19,15 @@ public class NvrliftBaseModule : AssettoServerModule<NvrliftBaseConfiguration> /
     
     protected override void Load(ContainerBuilder builder)
     {
-        builder.RegisterType<RestartModule>().AsSelf().SingleInstance();
+        /*
+        if(_configuration.Restart == RestartType.Disabled) { }
+        else if (_configuration.Restart == RestartType.WindowsFile)
+            builder.RegisterType<WindowsFileRestartImplementation>().As<IRestartImplementation>().SingleInstance();
+        else if (_configuration.Restart == RestartType.Docker)
+            builder.RegisterType<DockerRestartImplementation>().As<IRestartImplementation>().SingleInstance();
+            */
+        
+        builder.RegisterType<WindowsFileRestartImplementation>().As<IRestartImplementation>().SingleInstance();
 
         builder.RegisterType<TrackImplementation>().AsSelf().SingleInstance();
         builder.RegisterType<TrackManager>().AsSelf().SingleInstance();
