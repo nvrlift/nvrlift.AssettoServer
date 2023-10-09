ec6ca1bfb9e2ecd6cad6031166e99ed264df50d1
Nico <nico.macioszek@gmail.com>
2023-10-09 12:28:15 +0200

    Readding restart...

--- Track/TrackImplementation.cs
+++ Track/TrackImplementation.cs
@@ -3,6 +3,7 @@ using AssettoServer.Server.Configuration;
 using IniParser;
 using IniParser.Model;
 using nvrlift.AssettoServer.ContentManager;
+using nvrlift.AssettoServer.Restart;
 using Polly;
 using Serilog;
 
@@ -10,6 +11,7 @@ namespace nvrlift.AssettoServer.Track;
 
 public class TrackImplementation
 {
+    private readonly IRestartImplementation _restartImplementation;
     private readonly ContentManagerImplementation _contentManagerImplementation;
     private readonly ACServerConfiguration _acServerConfiguration;
     private readonly SessionManager _sessionManager;
@@ -18,12 +20,13 @@ public class TrackImplementation
 
     public TrackImplementation(ContentManagerImplementation contentManagerImplementation,
         SessionManager sessionManager,
-        ACServerConfiguration acServerConfiguration, EntryCarManager entryCarManager, ChecksumManager checksumManager)
+        ACServerConfiguration acServerConfiguration, EntryCarManager entryCarManager, ChecksumManager checksumManager, IRestartImplementation restartImplementation)
     {
         _contentManagerImplementation = contentManagerImplementation;
         _acServerConfiguration = acServerConfiguration;
         _entryCarManager = entryCarManager;
         _checksumManager = checksumManager;
+        _restartImplementation = restartImplementation;
         _sessionManager = sessionManager;
     }
 
@@ -59,20 +62,21 @@ public class TrackImplementation
             else
                 Log.Error("Failed to update ContentManager configuration.");
 
-        // Notify about restart
-        Log.Information($"Restarting server");
         
         // Next Session
         Log.Information("Reconnecting all clients track change.");
         foreach (var client in _entryCarManager.EntryCars.Select(c => c.Client))
         {
-            if (client != null)
-            {
-                client.Logger.Information("Reconnecting {ClientName}", client.Name);
-                client?.SendPacket(new LuaReconnectClients());
-            }
+            if (client == null) continue;
+            _entryCarManager.KickAsync(client, "SERVER RESTART").GetAwaiter().GetResult();
+            Log.Information($"Kicking {client.Name} for Server reset.");
+            //client?.SendPacket(new LuaReconnectClients());
         }
-        _checksumManager.Initialize();
-        _sessionManager.NextSession();
+        // Notify about restart
+        Log.Information($"Restarting server");
+        _restartImplementation.InitiateRestart();
+        
+        // _checksumManager.Initialize();
+        // _sessionManager.NextSession();
     }
 }
